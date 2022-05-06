d53a36e230a0765e20eb0585fdc19256d65bc084
ElioPantoja <elio.pantoja@outlook.com>
2022-05-06 13:36:10 -0300

    terminada la calc de descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,8 +1,22 @@
-const precioOriginal = 100;
-const descuento = 15;
+/* const precioOriginal = 100;
+const descuento = 15; */
+
 
 function calcPrecioConDescuento(precio, descuento){
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio*porcentajePrecioConDescuento)/100;
-return precioConDescuento;
+    return precioConDescuento;
+}
+
+function priceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+    
+    const precioConDescuento = calcPrecioConDescuento(priceValue,discountValue)
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio final es: $" + precioConDescuento
+    
 }
