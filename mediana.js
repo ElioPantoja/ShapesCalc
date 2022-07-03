b3ec8a40297deb05d78732015f0b0ba245e47388
ElioPantoja <elio.pantoja@outlook.com>
2022-07-03 16:55:24 -0300

    ya funciona el calculo de mediana

--- mediana.js
+++ mediana.js
@@ -6,30 +6,33 @@ function promedio (list){
 //+++++++++++++++++++++++++++++++++++++++++++++++++
 
 //let list = [123,4,554,67865,95]
-function inputList(list){
+function calcularMediana(list){
     list.sort((a,b)=> a-b)
-    return list
-}
 
-let mitadlista = parseInt(list.length / 2)
+    let mitadlista = parseInt(list.length / 2)
 
-function esPar(number){
-    if (list.length % 2 === 0){
-        return true
-    }else{
-        return false
+    function esPar(number){
+        if (list.length % 2 === 0){
+            console.log("Par")
+            return true
+        }else{
+            console.log("impar")
+            return false
+        }
     }
+
+    let mediana;
+
+    if (esPar (list.length)){
+        const elemento1 = list[mitadlista-1]
+        const elemento2 = list[mitadlista]
+        const promedioElementos = promedio[elemento1,elemento2]
+        mediana= promedio
+    }else{
+        mediana = list[mitadlista]
+    }
+
+    return mediana
+    
 }
-
-let mediana;
-
-if (esPar (list.length)){
-    const elemento1 = list[mitadlista-1]
-    const elemento2 = list[mitadlista]
-    const promedio = promedio[elemento1,elemento2]
-    mediana= promedio
-}else{
-    mediana = list[mitadlista]
-}
-
  
